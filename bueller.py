555b7b8e27bbb65d0ec8af24d1f7ee4ad2e4f2bd
rrcobb <rwcobbjr@gmail.com>
2015-08-05 18:25:41 -0700

    Added solution

--- bueller.py
+++ bueller.py
@@ -1,1 +1,17 @@
-# Add your code here
+def sort_by_last_name(names):
+    # create a list for the result
+    result = []
+
+    # loop through the names, modifying each and adding it
+    # to the result array
+    for name in names:
+        first_and_last = name.split(" ")
+        last_and_first = reversed(first_and_last)
+        name = ", ".join(last_and_first)
+        result.append(name)
+
+    # Sort alphabetically - last name is now at front of string
+    result.sort()
+
+    # Return the result list
+    return result
